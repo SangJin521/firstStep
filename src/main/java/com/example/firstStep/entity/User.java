5a3c79902c06e48806d234a34d4f8626237d1e85
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-01-08 17:56:54 +0900

    add dto,service

---
+++ src/main/java/com/example/firstStep/entity/User.java
@@ -0,0 +1,19 @@
+package com.example.firstStep.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
